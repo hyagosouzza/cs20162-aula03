78c80cb439eb3a5af2962f1f2fff11ffc6581cde
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 11:38:47 -0200

    Arrumando espaçamento

--- Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/RaizQuad.java
+++ Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/RaizQuad.java
@@ -2,15 +2,17 @@
  * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
  * Creative Commons Attribution 4.0 International License.
  */
+
 package com.github.hyagosouzza.cs20162aula03.exercicios2;
-/**Classe para utilizar o método raizQuadrada.
- * @author Hyago Vieira de Souza
+
+/**
+ * Classe para utilizar o método raizQuadrada.
  * @version 1.1
  */
 public class RaizQuad {
-    /**Metodo para retornar a raiz quadrada.
-     * @author Hyago Vieira de Souza
-     * @since Relase 01 da aplicação
+    
+    /**
+     * Metodo para retornar a raiz quadrada.
      * @param n final int
      * @param i final int
      * @return double - r
