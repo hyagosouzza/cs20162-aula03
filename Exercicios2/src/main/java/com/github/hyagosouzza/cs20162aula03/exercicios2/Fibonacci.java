e1064d9aaccad6e9eb8ba52a48c364992f6e2d2c
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 11:34:52 -0200

    Arrumando espaçamento

--- Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/Fibonacci.java
+++ Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/Fibonacci.java
@@ -2,15 +2,17 @@
  * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
  * Creative Commons Attribution 4.0 International License.
  */
+
 package com.github.hyagosouzza.cs20162aula03.exercicios2;
-/**Classe para utilizar o método fibo.
- * @author Hyago Vieira de Souza
+
+/**
+ * Classe para utilizar o método fibo.
  * @version 1.1
  */
 public class Fibonacci {
-    /**Metodo para obtenção do n-ésimo número de Fibonacci.
-     * @author Hyago Vieira de Souza
-     * @since Relase 01 da aplicação
+    
+    /**
+     * Metodo para obtenção do n-ésimo número de Fibonacci.
      * @param n final int
      * @return int - c
      */
