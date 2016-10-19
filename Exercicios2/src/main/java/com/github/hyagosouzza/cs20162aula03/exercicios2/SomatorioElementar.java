cb8301fbeb02497a1906a081b710c09bc1ceca95
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 15:51:14 -0200

    Update SomatorioElementar.java

--- Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/SomatorioElementar.java
+++ Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/SomatorioElementar.java
@@ -9,8 +9,8 @@ package com.github.hyagosouzza.cs20162aula03.exercicios2;
  * Classe para utilizar o método somaNaturais.
  * @version 1.1
  */
-public class SomatorioElementar
-{
+public class SomatorioElementar {
+
     /**
      * Metodo para retornar a somatória envolvendo números naturais.
      * @param n final int
