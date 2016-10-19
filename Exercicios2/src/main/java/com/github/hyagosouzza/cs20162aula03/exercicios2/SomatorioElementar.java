53ab184c68aa40f465a1eb8235df2534b2ce981d
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 11:42:24 -0200

    Arrumando espaçamento

--- Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/SomatorioElementar.java
+++ Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/SomatorioElementar.java
@@ -2,15 +2,17 @@
  * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
  * Creative Commons Attribution 4.0 International License.
  */
+
 package com.github.hyagosouzza.cs20162aula03.exercicios2;
-/**Classe para utilizar o método somaNaturais.
- * @author Hyago Vieira de Souza
+
+/**
+ * Classe para utilizar o método somaNaturais.
  * @version 1.1
  */
 public class SomatorioElementar {
-    /**Metodo para retornar a somatória envolvendo números naturais.
-     * @author Hyago Vieira de Souza
-     * @since Relase 01 da aplicação
+    
+    /**
+     * Metodo para retornar a somatória envolvendo números naturais.
      * @param n final int
      * @return double - s - soma
      */
