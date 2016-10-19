ea2248a159d5e8a57e77a752d40cc585eca94887
hyagosouzza <hyagosouzza@hotmail.com>
2016-10-19 15:09:00 -0200

    Update LogaritmoNatural.java

--- Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/LogaritmoNatural.java
+++ Exercicios2/src/main/java/com/github/hyagosouzza/cs20162aula03/exercicios2/LogaritmoNatural.java
@@ -9,8 +9,8 @@ package com.github.hyagosouzza.cs20162aula03.exercicios2;
  * Classe para utilizar o método logNat.
  * @version 1.1
  */
-public class LogaritmoNatural {
-    
+public class LogaritmoNatural
+{
     /**
      * Metodo para retornar o fatorial de n.
      * @param n final int
